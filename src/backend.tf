69d29a6bd594db45607ee4932ed6e8d863e96fa5
Diya Moliki <59070900+Diya-Moliki@users.noreply.github.com>
2023-02-26 21:34:57 -0500

    Delete backend.tf

--- src/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "mybucket-diya"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform-state-locking"
-    encrypt = true
-  }
-}
