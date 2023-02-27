637ae7ff629223fad9c34bfcd884ad3def862f6f
Diya Moliki <59070900+Diya-Moliki@users.noreply.github.com>
2023-02-26 21:10:24 -0500

    Create backend.tf

---
+++ src/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket          = "mybucket-diya"
+    key             = "terraform.tfstate"
+    region          = "us-east-1"
+    dynamodb_table  = "terraform-state-locking"
+    encrypt         = true
+  }
+}
