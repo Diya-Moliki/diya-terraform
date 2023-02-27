e2e216731af504b35f14c83faf4adc17643225a8
Diya Moliki <59070900+Diya-Moliki@users.noreply.github.com>
2023-02-26 22:41:15 -0500

    Update s3dyna.tf

--- src/modules/tf-state-resource/s3dyna.tf
+++ src/modules/tf-state-resource/s3dyna.tf
@@ -1,20 +1,3 @@
-
-# S3 Bucket for TF State File
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "mybucket-diya"
-}
-
-# Dynamo DB Table for Locking TF Config
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locking"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
