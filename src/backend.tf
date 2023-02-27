078c6b8ebdb7dfe15efccf0e7cb064d396da9c26
Diya Moliki <59070900+Diya-Moliki@users.noreply.github.com>
2023-02-26 21:32:19 -0500

    Update backend.tf

--- src/backend.tf
+++ src/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket          = "mybucket-diya"
-    key             = "terraform.tfstate"
-    region          = "us-east-1"
-    dynamodb_table  = "terraform-state-locking"
-    encrypt         = true
+    bucket = "mybucket-diya"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt = true
   }
 }
