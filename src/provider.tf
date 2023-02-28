70e39e87d2f705e2fcc6142397e0d79319d28ad2
Diya Moliki <59070900+Diya-Moliki@users.noreply.github.com>
2023-02-27 23:35:11 -0500

    Update provider.tf

--- src/provider.tf
+++ src/provider.tf
@@ -13,11 +13,11 @@ provider "aws" {
   version = "~> 4.0"
   region  = "us-east-1"
 }
-# terraform {
-#   backend "s3" {
-#     bucket         = "mybucket-diya" # Change this to a unique bucket name
-#     key            = "terraform.tfstate"
-#     region         = "us-east-1"               # Change this to your desired region
-#     dynamodb_table = "terraform-state-locking" # Change this to a unique DynamoDB table name
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "mybucket-diya" # Change this to a unique bucket name
+    key            = "terraform.tfstate"
+    region         = "us-east-1"               # Change this to your desired region
+    dynamodb_table = "terraform-state-locking" # Change this to a unique DynamoDB table name
+  }
+}
